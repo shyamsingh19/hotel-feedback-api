cf7ea494c477dd52e73b2a6338a5a431488685e0
shyamsingh19 <shyamsunders507@gmail.com>
2025-08-05 22:23:28 +0530

    written CRUD to update db

---
+++ app/crud.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+def create_feedback(db: Session, feedback: schemas.FeedbackCreate):
+    db_feedback = models.Feedback(**feedback.dict())
+    db.add(db_feedback)
+    db.commit()
+    db.refresh(db_feedback)
+    return db_feedback
+
+
+def get_all_feedback(db: Session):
+    return db.query(models.Feedback).all()
